0cb8c3591670ec717112b75bf7aaf6f333522a4e
mljohn73 <mxmxm11n77@gmail.com>
2025-08-02 17:30:59 -0400

    Adding more organization to side tray

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,25 +1,73 @@
 <script setup lang="ts">
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
   SidebarHeader,
 } from '@/components/ui/sidebar'
 import DarkMode from './DarkMode.vue';
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 </script>
 
 <template>
   <Sidebar>
     <SidebarHeader>
-      <div>
-        <DarkMode/>
-      </div>
     </SidebarHeader>
     <SidebarContent>
       <SidebarGroup />
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton asChild>
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{item.title}}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
       <SidebarGroup />
     </SidebarContent>
-    <SidebarFooter />
+    <SidebarFooter>
+      <div>
+        <DarkMode/>
+      </div>
+    </SidebarFooter>
   </Sidebar>
 </template>
